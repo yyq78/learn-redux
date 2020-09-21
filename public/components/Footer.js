10dc5d5883999d715a302285764b9dac67da86ca
yangyuqing <yangyuqing03@xdfzx.com>
2020-09-21 09:10:48 +0800

    添加tyoescript

---
+++ public/components/Footer.js
@@ -0,0 +1,26 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var react_1 = __importDefault(require("react"));
+var FilterLink_1 = require("../container/FilterLink");
+function Footer(_a) {
+    var filterLink = _a.filterLink, setFilterLink = _a.setFilterLink;
+    return (<p>
+            Show:
+            {' '}
+            <FilterLink_1.FilterLink filter="SHOW_ALL">
+                ALL
+            </FilterLink_1.FilterLink>
+            {', '}
+            <FilterLink_1.FilterLink filter="SHOW_ACTIVE">
+                Active
+            </FilterLink_1.FilterLink>
+            {', '}
+            <FilterLink_1.FilterLink filter="SHOW_COMPLETED">
+                Complete
+            </FilterLink_1.FilterLink>
+        </p>);
+}
+exports.default = Footer;
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
-import Root from './components/Root'
+import Root from './components/Root';
 import { createStore } from 'redux';
 import reducers from './reducers';
 const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
